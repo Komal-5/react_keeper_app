47bc3902573bf7702df0620d9e98c5689e131381
Komal-5 <komal.tater.05@gmail.com>
2022-04-26 20:05:30 +0530

    Updates

---
+++ precache-manifest.40eb0295ada1cd3acaa876d23c890d5d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee509555d4fbaf559f928cb31c34631",
+    "url": "/react_keeper_app/index.html"
+  },
+  {
+    "revision": "e8b8215853643c171cd1",
+    "url": "/react_keeper_app/static/js/2.134e8cae.chunk.js"
+  },
+  {
+    "revision": "3ba5202e4c41db2a2cf4",
+    "url": "/react_keeper_app/static/js/main.952216cb.chunk.js"
+  },
+  {
+    "revision": "56295c8d8a925c9e5faa",
+    "url": "/react_keeper_app/static/js/runtime-main.88db9795.js"
+  }
+]);
